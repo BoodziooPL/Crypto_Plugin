6a430e1f4b021b3a5dd1c0f47fac716eac2a1df2
B00210 <bogdanmalik@gmail.com>
2022-12-17 02:55:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const loginToWallet =  () => {
-    
+// funkcja podpinajaca metamask do strony i logujaca w konsoli adres walletu
 window.ethereum.enable().then(async function(accounts) {
     await console.log(accounts)
   });
